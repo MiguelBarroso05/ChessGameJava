32ea90f9256c243c75d5fcf193c261e8b8bbd154
Miguel Barroso <miguelreisbarroso@gmail.com>
2025-08-18 16:59:45 +0100

    create Board and Piece class

--- src/main/java/com/github/miguelbarroso05/Main.java
+++ src/main/java/com/github/miguelbarroso05/Main.java
@@ -1,10 +1,10 @@
 package com.github.miguelbarroso05;
 
-import com.github.miguelbarroso05.boardgame.Position;
+import com.github.miguelbarroso05.boardgame.Board;
 
 public class Main {
     public static void main(String[] args) {
-        Position position = new Position(3, 5);
-        System.out.println("Initial Position: " + position);
+        Board board = new Board(8, 8);
+
     }
 }
