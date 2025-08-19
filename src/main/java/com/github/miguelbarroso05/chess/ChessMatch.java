8966fef0ef35a0ba1c8711c98ddd98215475a7a5
Miguel Barroso <miguelreisbarroso@gmail.com>
2025-08-19 10:25:28 +0100

    add move validations in ChessMatch

--- src/main/java/com/github/miguelbarroso05/chess/ChessMatch.java
+++ src/main/java/com/github/miguelbarroso05/chess/ChessMatch.java
@@ -28,8 +28,15 @@ public class ChessMatch {
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
         validateSourcePosition(source);
+        validateTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
         return (ChessPiece) capturedPiece;
+    }
+
+    private void validateTargetPosition(Position source, Position target) {
+        if (!board.piece(source).possibleMove(target)) {
+            throw new ChessException("The chosen piece can't move to the location chose.");
+        }
     }
 
     private Piece makeMove(Position source, Position target) {
@@ -42,6 +49,9 @@ public class ChessMatch {
     private void validateSourcePosition(Position position) {
         if (!board.thereIsAPiece(position)) {
             throw new ChessException("There is no piece on source position");
+        }
+        if (!board.piece(position).isThereAnyPossibleMove()) {
+            throw new ChessException("There is no possible moves for the chosen piece");
         }
     }
 
